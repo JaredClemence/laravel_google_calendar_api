d451425cf7d3684599a34a50364e8f83c2b57d87
Jared R Clemence <jaredclemence@pediatricheartcenter.org>
2018-04-05 09:58:20 -0700

    First service provider.

---
+++ src/GoogleCalendarApiProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JRC\Google\Calendar;
+
+use Illuminate\Support\ServiceProvider;
+
+class GoogleCalendarApiProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
