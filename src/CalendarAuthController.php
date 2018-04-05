5a5da8ed3e5e1f3dcec53ae91d232e3588c7b3f7
Jared R Clemence <jaredclemence@pediatricheartcenter.org>
2018-04-05 10:30:25 -0700

    Added a controller

---
+++ src/CalendarAuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace JRC\Google\Calendar;
+
+use Illuminate\Http\Request;
+
+class CalendarAuthController extends Controller
+{
+    public function getAuthUrl(){
+        die( __DIR__ );
+    }
+}
